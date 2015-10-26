0ca449f960a31202b15c08aab34493a5f304de43
Caio SBA <caiosba@gmail.com>
2015-10-26 10:04:16 -0300

    Ticket #4292: Reapplying template

--- config/initializers/info.rb
+++ config/initializers/info.rb
@@ -2,5 +2,5 @@
     title: 'Lapis Example',
     description: 'An application to be used as an example for Lapis framework',
     author: 'Meedan',
-    author_url: 'http://meedan.com'
+    author_email: 'hello@meedan.com'
   }
