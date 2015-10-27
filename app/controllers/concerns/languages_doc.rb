130bce9b397f570897f5cf3954fd22296c54004d
Caio SBA <caiosba@gmail.com>
2015-10-27 02:14:30 -0300

    Ticket #4292: Applying new version of the template, updating Swagger documentation for new version and generating a client gem using "rake lapis:build_client_gem"

--- app/controllers/concerns/languages_doc.rb
+++ app/controllers/concerns/languages_doc.rb
@@ -9,9 +9,12 @@ module LanguagesDoc
       summary 'Send some text to be classified'
       notes 'Use this method in order to identify the language of a given text'
       param :query, :text, :string, :required, 'Text to be classified'
-      response :ok, 'Returns text language'
-      response 400, 'Parameters missing (text was not provided)'
-      response 401, 'Access denied'
+      # response(code, message, exampleRequest)
+      # "exampleRequest" should be: { query: {}, headers: {}, body: {} }
+      authed = { 'Authorization' => 'Token token="test"' }
+      response :ok, 'Text language', { query: { text: 'The book is on the table' }, headers: authed }
+      response 400, 'Parameter "text" is missing', { query: nil, headers: authed }
+      response 401, 'Access denied', { query: { text: 'Test' } }
     end
   end
 end
