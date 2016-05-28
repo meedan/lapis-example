ab4b44e3bc9f1eca3078c6cdc0f73b02fda48a11
Caio SBA <caiosba@gmail.com>
2016-05-28 10:39:10 -0300

    Ticket #4753: Use latest Lapis version

--- app/controllers/api/v1/languages_controller.rb
+++ app/controllers/api/v1/languages_controller.rb
@@ -7,8 +7,16 @@ class Api::V1::LanguagesController < Api::V1::BaseApiController
     if params[:text].blank?
       render_parameters_missing
     else
-      @language = wl.language(params[:text].to_s)
+      text = self.normalize(params[:text].to_s)
+      @language = wl.language(text)
       render_success 'language', @language
     end
+  end
+
+  protected
+
+  # @expose
+  def normalize(text)
+    text.downcase.gsub(/àáãâ/, 'a').gsub(/èéẽê/, 'e').gsub(/íìĩî/, 'i').gsub(/óòõô/, 'o').gsub(/úùũû/, 'u').gsub('ç', 'c')
   end
 end
--- langid_client/README.md
+++ langid_client/README.md
@@ -33,6 +33,10 @@ If you are going to test something that uses the 'langid_client' service, first 
 * LangidClient::Mock.mock_languages_classify_returns_parameter_text_is_missing
 * LangidClient::Mock.mock_languages_classify_returns_access_denied
 
+You can also reuse utility functions that are exposed by 'langid_client'. They are:
+
+* LangidClient::Util.normalize
+
 ## Contributing
 
 1. Fork it
